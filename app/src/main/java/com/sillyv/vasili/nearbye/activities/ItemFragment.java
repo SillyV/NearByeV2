beddde3bbc2148e01d8a1c61d387b24fc2f5a105
vasili <zooadmins@gmail.com>
2016-05-27 00:12:23 +0300

    C

--- app/src/main/java/com/sillyv/vasili/nearbye/activities/ItemFragment.java
+++ app/src/main/java/com/sillyv/vasili/nearbye/activities/ItemFragment.java
@@ -82,6 +82,7 @@ public class ItemFragment extends Fragment implements AbsListView.OnItemClickLis
         // TODO: Change Adapter to display your content
         mAdapter = new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
                 android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS);
+        //TEST
     }
 
     @Override
