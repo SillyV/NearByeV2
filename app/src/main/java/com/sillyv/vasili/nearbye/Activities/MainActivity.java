402a876c395b6dfd7c3734baa88f3106c9ca90ab
SilltV <vasili.fedotov@gmail.com>
2016-06-22 12:48:32 +0300

    olkik

--- app/src/main/java/com/sillyv/vasili/nearbye/Activities/MainActivity.java
+++ app/src/main/java/com/sillyv/vasili/nearbye/Activities/MainActivity.java
@@ -2,9 +2,12 @@ package com.sillyv.vasili.nearbye.activities;
 
 import android.Manifest;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -21,6 +24,7 @@ import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -53,25 +57,137 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
 {
     private static final String TAG = "SillyV.MainActivity";
     private static final int PLACE_PICKER_REQUEST = 40001;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 6615;
     private ResultsFragment resultsFragment;
     private MapFragment mapFragment;
     private GoogleApiClient mGoogleApiClient;
     private SupportMapFragment map;
     MenuItem searchMenuItem;
     private Toolbar mToolBar;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private boolean searchRequested = false;
+    private boolean locationFound = false;
+    private boolean initialSearchPerformed = false;
+    private Location myLocation;
+    private String myQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mapFragment = (MapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
         setUpActivityControls();
         mappingSetup();
+        LocationSetup();
+    }
+
+    private void LocationSetup()
+    {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener()
+        {
+            public void onLocationChanged(Location location)
+            {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+            }
+            else
+            {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+                }
+                else
+                {
+
+                    finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void makeUseOfNewLocation(Location location)
+    {
+        myLocation = location;
+        locationFound = true;
+        if (searchRequested)
+        {
+            initiateSearch();
+        }
+        else if (!initialSearchPerformed)
+        {
+            resultsFragment.SearchQueryINGooglePlacesWebService(myLocation);
+            initialSearchPerformed = true;
+        }
+    }
+
+    private void initiateSearch()
+    {
+        searchRequested = false;
+        initialSearchPerformed = true;
+        resultsFragment.SearchQueryINGooglePlacesWebService(myQuery, myLocation);
+        Log.d(TAG, "location found and search started");
     }
 
     private void mappingSetup()
@@ -123,7 +239,12 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
             @Override
             public boolean onQueryTextSubmit(String query)
             {
-                resultsFragment.SearchQueryINGooglePlacesWebService(query);
+                myQuery = query;
+                searchRequested = true;
+                if (locationFound)
+                {
+                    initiateSearch();
+                }
                 return false;
             }
 
@@ -146,7 +267,6 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
                 {
                     goToList();
                     getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    //// TODO: 23-May-16 Call functions to change Fragment Visibility
                 }
                 return true;
             }
@@ -196,59 +316,64 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
         return params;
     }
 
-
     private void goToMaps()
     {
-           FragmentManager fm = getSupportFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
 
-        fm.beginTransaction()
-                .show(mapFragment)
-                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                .commit();
-
-        fm.beginTransaction()
-                .hide(resultsFragment)
-                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                .commit();
+        showTransaction(mapFragment, fm);
+        hideTransaction(resultsFragment, fm);
     }
-
 
     private void goToList()
     {
         FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction()
-                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                .hide(mapFragment)
-                .commit();
-        fm.beginTransaction()
-                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                .show(resultsFragment)
-                .commit();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        resultsFragment.startingSettings(LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient));
-
-
+        hideTransaction(mapFragment, fm);
+        showTransaction(resultsFragment, fm);
     }
 
+    private void goToPrefrences()
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        hideTransaction(resultsFragment, fm);
+        hideTransaction(mapFragment, fm);
+    }
+
+    private void goToFavorites()
+    {
+        goToList();
+        resultsFragment.goToFavorites();
+    }
+
+    private void showTransaction(Fragment fragment, FragmentManager fm)
+    {
+        fm.beginTransaction()
+                .show(fragment)
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .commit();
+    }
+
+    private void hideTransaction(Fragment fragment, FragmentManager fm)
+    {
+        fm.beginTransaction()
+                .hide(fragment)
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .commit();
+    }
 
     @Override
     public void onFragmentInteraction(Results results)
     {
-        mapFragment.setLocation(results,this);
+        mapFragment.setLocation(results, this);
         searchMenuItem.collapseActionView();
         getSupportActionBar().setTitle(results.getName());
         goToMaps();
+    }
+
+    @Override
+    public void setListVisible()
+    {
+        goToList();
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
     @Override
@@ -268,7 +393,6 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
     {
 
     }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap)
@@ -291,7 +415,6 @@ public class MainActivity extends AppCompatActivity implements ResultsFragment.O
         }
         googleMap.setMyLocationEnabled(true);
     }
-
 
 
 }
